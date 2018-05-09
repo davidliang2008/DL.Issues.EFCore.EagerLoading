364b0a7bea6a54e968f0122ff67fdbfa54e7fbd0
David Liang <david.liang@outlook.com>
2018-05-09 16:52:59 -0700

    Add web project.

---
+++ Presentation/Web.UI/Startup.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Web.UI
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; private set; }
+
+        public Startup(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
