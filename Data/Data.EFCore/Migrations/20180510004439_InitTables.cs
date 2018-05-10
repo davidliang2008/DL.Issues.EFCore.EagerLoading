3a99226abafe5d60be9f7a96801329be71939bc4
David Liang <david.liang@outlook.com>
2018-05-09 17:59:01 -0700

    Setup seed data and ensure the database gets recreated each time the program runs.

---
+++ Data/Data.EFCore/Migrations/20180510004439_InitTables.cs
@@ -0,0 +1,203 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Data.EFCore.Migrations
+{
+    public partial class InitTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DomainEvent",
+                columns: table => new
+                {
+                    AggregateType = table.Column<string>(nullable: false),
+                    SequenceNumber = table.Column<int>(nullable: false),
+                    EntityId = table.Column<Guid>(nullable: false),
+                    EventType = table.Column<string>(nullable: false),
+                    TimeStamp = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DomainEvent", x => new { x.AggregateType, x.SequenceNumber });
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductType",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: false),
+                    Slug = table.Column<string>(nullable: false),
+                    Status = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductType", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductCategory",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: false),
+                    ProductTypeId = table.Column<Guid>(nullable: false),
+                    Slug = table.Column<string>(nullable: false),
+                    Status = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductCategory", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProductCategory_ProductType_ProductTypeId",
+                        column: x => x.ProductTypeId,
+                        principalTable: "ProductType",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: false),
+                    ProductCategoryId = table.Column<Guid>(nullable: false),
+                    Slug = table.Column<string>(nullable: false),
+                    Status = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Product_ProductCategory_ProductCategoryId",
+                        column: x => x.ProductCategoryId,
+                        principalTable: "ProductCategory",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductImage",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    IsThumbnail = table.Column<bool>(nullable: false),
+                    Ordinal = table.Column<int>(nullable: false),
+                    ProductId = table.Column<Guid>(nullable: false),
+                    Url = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductImage", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProductImage_Product_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Product",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductSpecification",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    DiscountedPrice = table.Column<double>(nullable: true),
+                    Price = table.Column<double>(nullable: false),
+                    ProductId = table.Column<Guid>(nullable: false),
+                    Value = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductSpecification", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProductSpecification_Product_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Product",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Product_Name",
+                table: "Product",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Product_ProductCategoryId",
+                table: "Product",
+                column: "ProductCategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Product_Slug",
+                table: "Product",
+                column: "Slug",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductCategory_Name",
+                table: "ProductCategory",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductCategory_ProductTypeId",
+                table: "ProductCategory",
+                column: "ProductTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductCategory_Slug",
+                table: "ProductCategory",
+                column: "Slug",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductImage_ProductId",
+                table: "ProductImage",
+                column: "ProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductSpecification_ProductId_Value",
+                table: "ProductSpecification",
+                columns: new[] { "ProductId", "Value" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductType_Name",
+                table: "ProductType",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductType_Slug",
+                table: "ProductType",
+                column: "Slug",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DomainEvent");
+
+            migrationBuilder.DropTable(
+                name: "ProductImage");
+
+            migrationBuilder.DropTable(
+                name: "ProductSpecification");
+
+            migrationBuilder.DropTable(
+                name: "Product");
+
+            migrationBuilder.DropTable(
+                name: "ProductCategory");
+
+            migrationBuilder.DropTable(
+                name: "ProductType");
+        }
+    }
+}
--- Presentation/Web.UI/Startup.cs
+++ Presentation/Web.UI/Startup.cs
@@ -1,8 +1,13 @@
-﻿using Data.EFCore.Extensions;
+﻿using Data.EFCore;
+using Data.EFCore.Entities;
+using Data.EFCore.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
 
 namespace Web.UI
 {
@@ -22,15 +27,294 @@ namespace Web.UI
             services.AddMvc();
         }
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env,
+            AppDbContext dbContext)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
+            SeedData(dbContext);
+
             app.UseStaticFiles();
             app.UseMvcWithDefaultRoute();
+        }
+
+        private void SeedData(AppDbContext context)
+        {
+            context.Database.EnsureDeleted();
+            context.Database.Migrate();
+
+            #region Create some product types
+
+            var cabinet = new ProductTypeEntity
+            {
+                Id = Guid.NewGuid(),
+                Name = "Cabinet",
+                Slug = "cabinet",
+                Status = Domain.ProductTypes.ProductTypeStatus.Published
+            };
+
+            var backsplash = new ProductTypeEntity
+            {
+                Id = Guid.NewGuid(),
+                Name = "Backsplash",
+                Slug = "backsplash",
+                Status = Domain.ProductTypes.ProductTypeStatus.Published
+            };
+
+            #endregion
+
+            #region Create some product categories
+
+            var wallCabinet = new ProductCategoryEntity
+            {
+                Id = Guid.NewGuid(),
+                Name = "Wall Cabinet",
+                Slug = "wall-cabinet",
+                Status = Domain.ProductCategories.ProductCategoryStatus.Published,
+                ProductType = cabinet
+            };
+
+            var vanityCabinet = new ProductCategoryEntity
+            {
+                Id = Guid.NewGuid(),
+                Name = "Vanity Cabinet",
+                Slug = "vanity-cabinet",
+                Status = Domain.ProductCategories.ProductCategoryStatus.Published,
+                ProductType = cabinet
+            };
+
+            var wallTileBacksplash = new ProductCategoryEntity
+            {
+                Id = Guid.NewGuid(),
+                Name = "Wall Tile Backsplash",
+                Slug = "wall-tile-backsplash",
+                Status = Domain.ProductCategories.ProductCategoryStatus.Published,
+                ProductType = backsplash
+            };
+
+            #endregion
+
+            #region Create some products
+
+            var bengalStoneWallTileBacksplash = new ProductEntity
+            {
+                Id = Guid.NewGuid(),
+                Name = "Bengal Stone Wall Tile Backsplash",
+                Slug = "bengal-stone-wall-tile-backsplash",
+                Status = Domain.Products.ProductStatus.Published,
+                ProductCategory = wallTileBacksplash,
+                ProductImages = new List<ProductImageEntity>
+                    {
+                        new ProductImageEntity
+                        {
+                            Id = Guid.NewGuid(),
+                            Url = "bengal-1",
+                            IsThumbnail = true,
+                            Ordinal = 1
+                        }
+                    },
+                ProductSpecifications = new List<ProductSpecificationEntity>
+                    {
+                        new ProductSpecificationEntity
+                        {
+                            Value = "bengal-specification-1",
+                            Price = 23.4
+                        },
+                        new ProductSpecificationEntity
+                        {
+                            Value = "bengal-specification-2",
+                            Price = 40,
+                            DiscountedPrice = 30
+                        },
+                        new ProductSpecificationEntity
+                        {
+                            Value = "bengal-specification-3",
+                            Price = 20,
+                            DiscountedPrice = 12.5
+                        }
+                    }
+            };
+
+            var metroCarreraWallTileBacksplash = new ProductEntity
+            {
+                Id = Guid.NewGuid(),
+                Name = "Metro Carrera Wall Tile Backsplash",
+                Slug = "metro-carrera-wall-tile-backsplash",
+                Status = Domain.Products.ProductStatus.Published,
+                ProductCategory = wallTileBacksplash,
+                ProductImages = new List<ProductImageEntity>
+                    {
+                        new ProductImageEntity
+                        {
+                            Id = Guid.NewGuid(),
+                            Url = "carrera-1",
+                            IsThumbnail = true,
+                            Ordinal = 1
+                        },
+                        new ProductImageEntity
+                        {
+                            Id = Guid.NewGuid(),
+                            Url = "carrera-2",
+                            IsThumbnail = false,
+                            Ordinal = 2
+                        }
+                    },
+                ProductSpecifications = new List<ProductSpecificationEntity>
+                    {
+                        new ProductSpecificationEntity
+                        {
+                            Value = "carrera-specification-1",
+                            Price = 23
+                        },
+                        new ProductSpecificationEntity
+                        {
+                            Value = "carrera-specification-2",
+                            Price = 20,
+                            DiscountedPrice = 10
+                        }
+                    }
+            };
+
+            var cabinetChoices = new ProductEntity
+            {
+                Id = Guid.NewGuid(),
+                Name = "Cabinet Choices",
+                Slug = "cabinet-choices",
+                Status = Domain.Products.ProductStatus.Published,
+                ProductCategory = wallCabinet,
+                ProductImages = new List<ProductImageEntity>
+                    {
+                        new ProductImageEntity
+                        {
+                            Id = Guid.NewGuid(),
+                            Url = "cabinet-choice-1",
+                            IsThumbnail = false,
+                            Ordinal = 1
+                        },
+                        new ProductImageEntity
+                        {
+                            Id = Guid.NewGuid(),
+                            Url = "cabinet-choice-2",
+                            IsThumbnail = true,
+                            Ordinal = 2
+                        },
+                        new ProductImageEntity
+                        {
+                            Id = Guid.NewGuid(),
+                            Url = "cabinet-choice-3",
+                            IsThumbnail = false,
+                            Ordinal = 3
+                        },
+                        new ProductImageEntity
+                        {
+                            Id = Guid.NewGuid(),
+                            Url = "cabinet-choice-4",
+                            IsThumbnail = false,
+                            Ordinal = 4
+                        },
+                        new ProductImageEntity
+                        {
+                            Id = Guid.NewGuid(),
+                            Url = "cabinet-choice-5",
+                            IsThumbnail = false,
+                            Ordinal = 5
+                        },
+                        new ProductImageEntity
+                        {
+                            Id = Guid.NewGuid(),
+                            Url = "cabinet-choice-6",
+                            IsThumbnail = false,
+                            Ordinal = 6
+                        }
+                    }
+            };
+
+            #endregion
+
+            context.ProductTypes.Add(cabinet);
+            context.DomainEvents.Add(new DomainEventEntity
+            {
+                AggregateType = "Domain.ProductTypes.ProductType",
+                SequenceNumber = 1,
+                EventType = "Domain.ProductTypes.Events.ProductTypeCreated",
+                EntityId = cabinet.Id,
+                TimeStamp = DateTime.UtcNow
+            });
+
+            context.ProductTypes.Add(backsplash);
+            context.DomainEvents.Add(new DomainEventEntity
+            {
+                AggregateType = "Domain.ProductTypes.ProductType",
+                SequenceNumber = 2,
+                EventType = "Domain.ProductTypes.Events.ProductTypeCreated",
+                EntityId = backsplash.Id,
+                TimeStamp = DateTime.UtcNow
+            });
+
+            context.ProductCategories.Add(wallCabinet);
+            context.DomainEvents.Add(new DomainEventEntity
+            {
+                AggregateType = "Domain.ProductCategories.ProductCategory",
+                SequenceNumber = 1,
+                EventType = "Domain.ProductCategories.Events.ProductCategoryCreated",
+                EntityId = wallCabinet.Id,
+                TimeStamp = DateTime.UtcNow
+            });
+
+            context.ProductCategories.Add(vanityCabinet);
+            context.DomainEvents.Add(new DomainEventEntity
+            {
+                AggregateType = "Domain.ProductCategories.ProductCategory",
+                SequenceNumber = 2,
+                EventType = "Domain.ProductCategories.Events.ProductCategoryCreated",
+                EntityId = vanityCabinet.Id,
+                TimeStamp = DateTime.UtcNow
+            });
+
+            context.ProductCategories.Add(wallTileBacksplash);
+            context.DomainEvents.Add(new DomainEventEntity
+            {
+                AggregateType = "Domain.ProductCategories.ProductCategory",
+                SequenceNumber = 3,
+                EventType = "Domain.ProductCategories.Events.ProductCategoryCreated",
+                EntityId = wallTileBacksplash.Id,
+                TimeStamp = DateTime.UtcNow
+            });
+
+            context.Products.Add(bengalStoneWallTileBacksplash);
+            context.DomainEvents.Add(new DomainEventEntity
+            {
+                AggregateType = "Domain.Products.Product",
+                SequenceNumber = 1,
+                EventType = "Domain.Products.Events.ProductCreated",
+                EntityId = bengalStoneWallTileBacksplash.Id,
+                TimeStamp = DateTime.UtcNow
+            });
+
+            context.Products.Add(metroCarreraWallTileBacksplash);
+            context.DomainEvents.Add(new DomainEventEntity
+            {
+                AggregateType = "Domain.Products.Product",
+                SequenceNumber = 2,
+                EventType = "Domain.Products.Events.ProductCreated",
+                EntityId = metroCarreraWallTileBacksplash.Id,
+                TimeStamp = DateTime.UtcNow
+            });
+
+            context.Products.Add(cabinetChoices);
+            context.DomainEvents.Add(new DomainEventEntity
+            {
+                AggregateType = "Domain.Products.Product",
+                SequenceNumber = 3,
+                EventType = "Domain.Products.Events.ProductCreated",
+                EntityId = cabinetChoices.Id,
+                TimeStamp = DateTime.UtcNow
+            });
+
+            context.SaveChanges();
         }
     }
 }
